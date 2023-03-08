6c1aaa57df8ad92a6b5ec7b236d38416394b333e
Fernando Cuellar <fercuellar@gmail.com>
2023-03-08 15:22:55 -0600

    Se termina la seccion de Funciones

--- 01-Fundamentos/assets/js/ciclos/for.js
+++ 01-Fundamentos/assets/js/ciclos/for.js
@@ -1,21 +1,27 @@
 
 const heroes = ['Batman', 'Superman', 'Mujer Maravilla', 'Aquaman'];
 
+// sintaxis basica de un for
+
 console.warn('For tradicional');
 for( let i = 0; i < heroes.length; i++ ) {
     console.log( heroes[i] );
-}
+};
 
+//  sintaxis basica simplificada mas limpio
 
 console.warn('For in');
 for( let i in heroes ) {
     console.log( heroes[i] );
-}
+};
+
+// se utiliza par areferencias y arreglos mas sencilla
 
 console.warn('For of');
 for( let heroe of heroes ){
     console.log( heroe );
-}
+};
+
 
 
 
